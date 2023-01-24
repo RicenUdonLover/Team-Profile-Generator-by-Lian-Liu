3e1f32ed180cf1f734935dd4cf9b9f2e175b4624
Lian Liu <riceudon@gmail.com>
2023-01-24 11:44:51 -0800

    added comments

--- index.js
+++ index.js
@@ -4,31 +4,38 @@ const { Employee, Manager, Engineer, Intern } = require('./lib/classes.js');
 const team = []
 
 
-
+// Prompts user for manager input.
 inquirer.prompt(managerQuestions).then(answers => {
-    const { name, id, email, officeNumber } = answers;
-    const manager = new Manager(name, id, email, officeNumber);
-    // console.log(manager);
+    const { name, id, email, officeNumber } = answers;  // Destructuring the answers object to extract the properties.
+    const manager = new Manager(name, id, email, officeNumber); //Creates manager object and adds to team array.
     team.push(manager);
-    return inquirer.prompt(buildTeam);
+    return inquirer.prompt(buildTeam); //Prompts user for next team member role.
 }).then((answers) => {
+    // Prompts user for engineer input, creates engineer object and adds to team array. Prompts user for next team member role.
     switch (answers.role) {
         case "Engineer":
-            inquirer.prompt(engineerQuestions).then(answers => {
+            return inquirer.prompt(engineerQuestions).then(answers => {
                 const { name, id, email, github } = answers;
                 const engineer = new Engineer(name, id, email, github);
                 team.push(engineer);
                 return inquirer.prompt(buildTeam);
-            });            
+            });
             break;
+        // Prompts user for intern input, creates intern object and adds to team array. Prompts user for next team member role.
         case "Intern":
-            inquirer.prompt(internQuestions).then(answers => {
+            return inquirer.prompt(internQuestions).then(answers => {
                 const { name, id, email, school } = answers;
-                const engineer = new Intern (name, id, email, school);
-                team.push(engineer);
+                const intern = new Intern(name, id, email, school);
+                team.push(intern);
                 return inquirer.prompt(buildTeam);
+            });
             break;
+        // Ends team building process, logs team array to console if more than one member, otherwise reminds user to add more members.
         default:
-            break
+            if (team.length === 1) {
+                console.log("There is only a manager, please add more team members.")
+                return inquirer.prompt(buildTeam);
+            } else { console.log(team) }
+            break;
     }
 });
