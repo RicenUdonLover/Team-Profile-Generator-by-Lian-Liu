e010015cbbb023deb172effb0a22cedd90cdd58f
Lian Liu <riceudon@gmail.com>
2023-01-24 11:29:31 -0800

    Removed addtoteam, engineer, intern, and teambuilder.js. Put all classes in classes.js

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 const inquirer = require("inquirer");
 const { managerQuestions, buildTeam, engineerQuestions, internQuestions } = require('./utils/questions.js')
+const { Employee, Manager, Engineer, Intern } = require('./lib/classes.js');
 const team = []
 
-const Manager = function (name, id, email, office) {
-    this.role = 'Manager'
-    this.name = name
-    this.id = id
-    this.email = email
-    this.officeNumber = office
-}
+
 
 inquirer.prompt(managerQuestions).then(answers => {
-    const {name, email, id, officeNumber} = answers
-    const manager = new Manager(name, id, email, officeNumber)
-    console.log(manager)
-    team.push(manager)
-})
-// .then(inquirer.prompt(buildTeam).then(answers => {
-//     switch(answers.role) {
-//         case "Engineer":
-//             console.log(answers);
-//             break;
-//         case "Intern":
-//             console.log(answers);
-//             break;
-//         default:
-
-//             break
-//     }
-// }))
+    const { name, id, email, officeNumber } = answers;
+    const manager = new Manager(name, id, email, officeNumber);
+    // console.log(manager);
+    team.push(manager);
+    return inquirer.prompt(buildTeam);
+}).then((answers) => {
+    switch (answers.role) {
+        case "Engineer":
+            inquirer.prompt(engineerQuestions).then(answers => {
+                const { name, id, email, github } = answers;
+                const engineer = new Engineer(name, id, email, github);
+                team.push(engineer);
+                return inquirer.prompt(buildTeam);
+            });            
+            break;
+        case "Intern":
+            inquirer.prompt(internQuestions).then(answers => {
+                const { name, id, email, school } = answers;
+                const engineer = new Intern (name, id, email, school);
+                team.push(engineer);
+                return inquirer.prompt(buildTeam);
+            break;
+        default:
+            break
+    }
+});
