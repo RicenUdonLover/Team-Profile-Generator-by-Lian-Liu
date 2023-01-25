21d24218e0b3ae907f834175ce66166515f9aeb7
Lian Liu <riceudon@gmail.com>
2023-01-24 22:14:26 -0800

    Separate the big function into smaller functions

--- index.js
+++ index.js
@@ -4,38 +4,66 @@ const { Employee, Manager, Engineer, Intern } = require('./lib/classes.js');
 const team = []
 
 
-// Prompts user for manager input.
-inquirer.prompt(managerQuestions).then(answers => {
-    const { name, id, email, officeNumber } = answers;  // Destructuring the answers object to extract the properties.
-    const manager = new Manager(name, id, email, officeNumber); //Creates manager object and adds to team array.
-    team.push(manager);
-    return inquirer.prompt(buildTeam); //Prompts user for next team member role.
-}).then((answers) => {
-    // Prompts user for engineer input, creates engineer object and adds to team array. Prompts user for next team member role.
-    switch (answers.role) {
-        case "Engineer":
-            return inquirer.prompt(engineerQuestions).then(answers => {
-                const { name, id, email, github } = answers;
-                const engineer = new Engineer(name, id, email, github);
-                team.push(engineer);
-                return inquirer.prompt(buildTeam);
-            });
-            break;
-        // Prompts user for intern input, creates intern object and adds to team array. Prompts user for next team member role.
-        case "Intern":
-            return inquirer.prompt(internQuestions).then(answers => {
-                const { name, id, email, school } = answers;
-                const intern = new Intern(name, id, email, school);
-                team.push(intern);
-                return inquirer.prompt(buildTeam);
-            });
-            break;
-        // Ends team building process, logs team array to console if more than one member, otherwise reminds user to add more members.
-        default:
-            if (team.length === 1) {
-                console.log("There is only a manager, please add more team members.")
-                return inquirer.prompt(buildTeam);
-            } else { console.log(team) }
-            break;
+
+const createTeam = async function () {
+    try {
+        const manager = await createManager();
+        team.push(manager);
+        while (true) {
+                const nextAnswers = await askTeamMember();
+                if (nextAnswers.role === "Engineer") {
+                    const engineer = await createEngineer();
+                    team.push(engineer);
+                } else if (nextAnswers.role === "Intern") {
+                    const intern = await createIntern();
+                    team.push(intern);
+                } else {
+                    if (team.length === 1) {
+                        console.log("There is only a manager, please add more team members.")
+                    } else {
+                        const proceedAnswers = await askProceed(JSON.stringify(team));
+                        if (proceedAnswers.proceed === "Create HTML") {
+                            console.log(`creating HTML`)
+                        }
+                        break;
+                    }
+                }
+            }
+        } catch (err) {
+        console.log(err);
     }
-});
+}
+
+const createManager = async function () {
+    const answers = await inquirer.prompt(managerQuestions);
+    const { name, id, email, officeNumber } = answers;
+    return new Manager(name, id, email, officeNumber);
+}
+
+const askProceed = async function (team) {
+    return inquirer.prompt({
+        type: "list",
+        name: "proceed",
+        message: `Your team looks like ${team}. What would you like to do?`,
+        choices: ["Create HTML", "Add more team members"]
+    });
+}
+
+const askTeamMember = async function () {
+    return inquirer.prompt(buildTeam);
+}
+
+const createEngineer = async function () {
+    const engineerAnswers = await inquirer.prompt(engineerQuestions);
+    const { name, id, email, github } = engineerAnswers;
+    return new Engineer(name, id, email, github);
+}
+
+const createIntern = async function () {
+    const internAnswers = await inquirer.prompt(internQuestions);
+    const { name, id, email, school } = internAnswers;
+    return new Intern(name, id, email, school);
+}
+
+
+createTeam()
