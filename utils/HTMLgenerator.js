f6b2a745ce8a79cbdf302deb6aebec6c65ca36d9
Lian Liu <riceudon@gmail.com>
2023-01-29 11:14:30 -0800

    add styling to html

--- utils/HTMLgenerator.js
+++ utils/HTMLgenerator.js
@@ -13,36 +13,38 @@ const generateHTML = (team) => {
   <body>
       <div class="container">
           <div class="row">`;
-
   for (const member of team) {
     html += `
-              <div class="col-sm-4">
-                  <div class="card">
-                      <div class="card-header">
-                          ${member.name} - ${member.getRole()}
-                      </div>
-                      <div class="card-body">
-                          <p>ID: ${member.id}</p>
-                          <p>Email: <a href="mailto:${member.email}">${member.email}</a>
-                          </p>`;
+                        <div class="col-sm-4">
+                            <div class="card bg-white">
+                                <div class="card-header bg-primary text-white">
+                                    ${member.name} - ${member.getRole()}
+                                </div>
+                                <div class="card-body">
+                                    <p>ID: ${member.id}</p>
+                                    <p>Email: <a href="mailto:${member.email}">${member.email}</a>
+                                    </p>`;
     if (member.getRole() === "Manager") {
       html += `<p>Office Number: ${member.officeNumber}</p>`;
     } else if (member.getRole() === "Engineer") {
       html += `<p>Github: <a href="https://github.com/${member.github}" target="_blank">${member.github}</a>
-      </p>`;
+                </p>`;
     } else if (member.getRole() === "Intern") {
       html += `<p>School: ${member.school}</p>`;
     }
     html += `
-                      </div>
-                  </div>
-              </div>`;
+                                </div>
+                            </div>
+                        </div>`;
   }
   html += `
-          </div>
-      </div>
-  </body>
-  </html>`;
+                    </div>
+                </div>
+                <footer class="bg-primary text-white p-4 text-center">
+                  <p>2023 Made by Lian Liu</p>
+                </footer>
+            </body>
+            </html>`;
 
   fs.writeFileSync("./dist/team.html", html);
 }
