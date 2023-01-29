732a9ccdb3b489b7a0dcdc0ada76524e4ca90a6b
Lian Liu <riceudon@gmail.com>
2023-01-29 11:22:56 -0800

    Some adjust to styling

--- utils/HTMLgenerator.js
+++ utils/HTMLgenerator.js
@@ -10,46 +10,48 @@ const generateHTML = (team) => {
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
       <title>My Team</title>
   </head>
-  <body class="bg-light">
-      <header class="bg-primary text-white p-4">
-        <h1 class="text-center">My Team</h1>
-      </header>
-      <div class="container">
+    <body class="bg-light">
+        <div class="container my-5">
+          <header class="text-center">
+              <h1 class="mb-5">${team[0].name}'s Team</h1>
+          </header>
           <div class="row">`;
-  for (const member of team) {
-    html += `
-                        <div class="col-sm-4">
-                            <div class="card bg-white">
-                                <div class="card-header bg-primary text-white">
-                                    ${member.name} - ${member.getRole()}
-                                </div>
-                                <div class="card-body">
-                                    <p>ID: ${member.id}</p>
-                                    <p>Email: <a href="mailto:${member.email}">${member.email}</a>
-                                    </p>`;
-    if (member.getRole() === "Manager") {
-      html += `<p>Office Number: ${member.officeNumber}</p>`;
-    } else if (member.getRole() === "Engineer") {
-      html += `<p>Github: <a href="https://github.com/${member.github}" target="_blank">${member.github}</a>
-                </p>`;
-    } else if (member.getRole() === "Intern") {
-      html += `<p>School: ${member.school}</p>`;
+  
+    for (const member of team) {
+      html += `
+                <div class="col-sm-4">
+                    <div class="card mb-4">
+                        <div class="card-header bg-primary text-white">
+                            ${member.name} - ${member.getRole()}
+                        </div>
+                        <div class="card-body">
+                            <p>ID: ${member.id}</p>
+                            <p>Email: <a href="mailto:${employee.email}">${employee.email}</a>
+                         </p>`;
+      if (member.getRole() === "Manager") {
+        html += `<p>Office Number: ${member.officeNumber}</p>`;
+      } else if (member.getRole() === "Engineer") {
+        html += `<p>Github: <a href="https://github.com/${employee.github}" target="_blank">${employee.github}</a>
+        </p>`;
+      } else if (member.getRole() === "Intern") {
+        html += `<p>School: ${member.school}</p>`;
+      }
+      html += `
+                        </div>
+                    </div>
+                </div>`;
     }
     html += `
-                                </div>
-                            </div>
-                        </div>`;
+            </div>
+            <footer class="text-center mt-5">
+              <p>Copyright © My Team</p>
+            </footer>
+        </div>
+    </body>
+    </html>`;
+  
+    fs.writeFileSync("./dist/team.html", html);
   }
-  html += `
-                    </div>
-                </div>
-                <footer class="bg-primary text-white p-4 text-center">
-                  <p>2023 Made by Lian Liu</p>
-                </footer>
-            </body>
-            </html>`;
-
-  fs.writeFileSync("./dist/team.html", html);
-}
-
-module.exports = generateHTML
+  
+  module.exports = generateHTML
+  
