9229c1585a407b299ca9945b4a5b5ee3f1b8fbe6
Lian Liu <riceudon@gmail.com>
2023-02-05 11:19:27 -0800

    Added validations to all questions

--- utils/questions.js
+++ utils/questions.js
@@ -1,31 +1,37 @@
+const checkEmpty = input => input.length !== 0 || `Your input cannot be empty.`
+
+const checkInteger = input => {
+    const isEmpty = input.length === 0
+    const isInteger = Number.isInteger(parseFloat(input))
+    return isEmpty ? `Your input cannot be empty.` : isInteger ? true : `Your input must be an integer.`;
+}
+
 const managerQuestions = [
-        {
-            type: "input",
-            name: "name",
-            message: "Enter team manager's name:",
-            validate: name => name.length !== 0 || "Name cannot be empty."
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "Enter team manager's employee ID:",
-            validate: id => {
-                id.length !== 0 || "ID cannot be empty."
-                const isInteger = Number.isInteger(parseInt(id))
-                return isInteger ? true : "ID must be an integer.";
-            }
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "Enter team manager's email address:"
-        },
-        {
-            type: "input",
-            name: "officeNumber",
-            message: "Enter team manager's office number:"
-        }
-    ]
+    {
+        type: "input",
+        name: "name",
+        message: "Enter team manager's name:",
+        validate: input => checkEmpty(input)
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "Enter team manager's employee ID:",
+        validate: input => checkInteger(input)
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter team manager's email address:",
+        validate: input => checkEmpty(input)
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "Enter team manager's office number:",
+        validate: input => checkInteger(input)
+    }
+]
 
 const buildTeam = [
     {
@@ -41,22 +47,25 @@ const engineerQuestions = [
         type: "input",
         name: "name",
         message: "Enter engineer's name:",
-        validate: name => name.length !== 0 || "Name cannot be empty."
+        validate: input => checkEmpty(input)
     },
     {
         type: "input",
         name: "id",
-        message: "Enter engineer's employee ID:"
+        message: "Enter engineer's employee ID:",
+        validate: input => checkInteger(input)
     },
     {
         type: "input",
         name: "email",
-        message: "Enter engineer's email address:"
+        message: "Enter engineer's email address:",
+        validate: input => checkEmpty(input)
     },
     {
         type: "input",
         name: "github",
-        message: "Enter engineer's GitHub username:"
+        message: "Enter engineer's GitHub username:",
+        validate: input => checkEmpty(input)
     }
 ]
 
@@ -65,22 +74,25 @@ const internQuestions = [
         type: "input",
         name: "name",
         message: "Enter intern's name:",
-        validate: name => name.length !== 0 || "Name cannot be empty."
+        validate: input => checkEmpty(input)
     },
     {
         type: "input",
         name: "id",
-        message: "Enter intern's employee ID:"
+        message: "Enter intern's employee ID:",
+        validate: input => checkInteger(input)
     },
     {
         type: "input",
         name: "email",
-        message: "Enter intern's email address:"
+        message: "Enter intern's email address:",
+        validate: input => checkEmpty(input)
     },
     {
         type: "input",
         name: "school",
-        message: "Enter intern's school:"
+        message: "Enter intern's school:",
+        validate: input => checkEmpty(input)
     }
 ]
 
